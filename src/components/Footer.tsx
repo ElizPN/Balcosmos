99bfa57bd4b91c3754aea3bad484bbe45e2dc0dc
ElizPN <pankevych.liza@gmail.com>
2022-12-07 18:54:20 +0100

    delete box

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,30 +12,28 @@ import { height } from "@mui/system";
 
 export default function Footer() {
   return (
-    <Box sx={{ height: "200" }}>
-      <Paper
-        sx={{ position: "fixed", bottom: 100, left: 0, right: 0 }}
-        elevation={3}
-      >
-        <BottomNavigation>
-          <BottomNavigationAction
-            label='Facebook'
-            icon={<Icon icon='ci:facebook' width='80' height='80' />}
-          />
-          <BottomNavigationAction
-            label='Youtube'
-            icon={<Icon icon='icomoon-free:youtube' width='60' height='60' />}
-          />
-          <BottomNavigationAction
-            label='Soundcloud'
-            icon={<Icon icon='mdi:soundcloud' width='80' height='80' />}
-          />
-          <BottomNavigationAction
-            label='Spotify'
-            icon={<Icon icon='fa6-brands:spotify' width='80' height='80' />}
-          />
-        </BottomNavigation>
-      </Paper>
-    </Box>
+    <Paper
+      sx={{ position: "fixed", bottom: 100, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation>
+        <BottomNavigationAction
+          label='Facebook'
+          icon={<Icon icon='ci:facebook' width='80' height='80' />}
+        />
+        <BottomNavigationAction
+          label='Youtube'
+          icon={<Icon icon='icomoon-free:youtube' width='60' height='60' />}
+        />
+        <BottomNavigationAction
+          label='Soundcloud'
+          icon={<Icon icon='mdi:soundcloud' width='80' height='80' />}
+        />
+        <BottomNavigationAction
+          label='Spotify'
+          icon={<Icon icon='fa6-brands:spotify' width='80' height='80' />}
+        />
+      </BottomNavigation>
+    </Paper>
   );
 }
