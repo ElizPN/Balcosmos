4613262030a24531c214c328175ada20afd1a8bd
ElizPN <pankevych.liza@gmail.com>
2022-12-21 14:10:18 +0100

    make icons bigger

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,42 +4,55 @@ import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Paper from "@mui/material/Paper";
 import { Icon } from "@iconify/react";
 import Link from "@mui/material/Link";
+import { styled } from "@mui/material/styles";
+
+const StyledPaper = styled (Paper) (() => ({
+  bottom: 100,
+  left: 0,
+  right: 0,
+  fontSize: 50,
+}));
+
+const StyledBottomNavigationAction = styled(BottomNavigationAction)(() => ({
+  width: 60,
+  height: 60,
+})) as typeof BottomNavigationAction;
 
 export default function Footer() {
   return (
-    <Paper sx={{  bottom: 100, left: 0, right: 0 }} elevation={3}>
+    <StyledPaper elevation={3}>
       <BottomNavigation>
-        <BottomNavigationAction
+        <StyledBottomNavigationAction
           href='https://www.facebook.com/Balcosmos'
           target='_blank'
           label='Facebook'
-          icon={<Icon icon='ci:facebook' width='80' height='80' />}
+          icon={<Icon icon='ci:facebook' />}
         />
-        <BottomNavigationAction
+        <StyledBottomNavigationAction
           href='https://www.youtube.com/@balcosmos'
           target='_blank'
           label='Youtube'
-          icon={<Icon icon='icomoon-free:youtube' width='100' height='100' />}
+          icon={<Icon icon='icomoon-free:youtube' />}
         />
-        <BottomNavigationAction
+        <StyledBottomNavigationAction
           href='https://soundcloud.com/balcosmos'
           target='_blank'
           label='Soundcloud'
-          icon={<Icon icon='mdi:soundcloud' width='100' height='100' />}
+          icon={<Icon icon='mdi:soundcloud' />}
         />
-        <BottomNavigationAction
+        <StyledBottomNavigationAction
           href='https://open.spotify.com/artist/5wiOTsRu9iNdLAaQL2Oea3'
           target='_blank'
           label='Spotify'
-          icon={<Icon icon='fa6-brands:spotify' width='100' height='100' />}
+          icon={<Icon icon='fa6-brands:spotify' />}
         />
-        <BottomNavigationAction
+        <StyledBottomNavigationAction
           href='https://balcosmos.bandcamp.com/'
           target='_blank'
           label='Bandcamp'
-          icon={<Icon icon='tabler:brand-bandcamp' width='100' height='100' />}
+          icon={<Icon icon='tabler:brand-bandcamp' />}
         />
       </BottomNavigation>
-    </Paper>
+    </StyledPaper>
   );
 }
