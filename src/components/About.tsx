54834e7e7afa2e31af34d4a98344af529acb7825
ElizPN <pankevych.liza@gmail.com>
2023-01-11 12:12:26 +0100

    add paragrraphs and title

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,31 +13,39 @@ export default function About() {
       marginBottom={60}
       id='biography'
     >
-      <Box sx={{ backgroundColor: "#000000cf", padding: "40px" }}>
+      <Grid item sx={{ backgroundColor: "#000000cf", padding: "40px" }}>
+        <Typography textAlign='center' variant='h5' mb={3}>
+          BIOGRAPHY
+        </Typography>
         <Typography
           variant='body2'
           textAlign={"justify"}
-          fontSize={20}
           maxWidth={900}
           width='100%'
         >
-          Originating from Ukraine, Alexey Romanov (38) is behind the solo
-          project “Balcosmos”. Influenced from a vast spectre of psytrance
-          sub-genres, including Goa, Forest, Fullon and Progressive, Alexey’s
-          initial Dj career evolved into the birth of his own music productions
-          under the project “Barbuix”. In 2017 he released his debut album
-          “Secret Ingredient” on the Russian based label Sound Kitchen records
-          and has since played on multiple parties and events in countries such
-          as Ukraine, Russia and Sweden. In 2020 his developing skills and
-          culmination of experiences, forged his newly formed project Balcosmos,
-          under which he is set to reconnect the audience with a fresh music
-          approach of pure psychedelic vibes, balancing cleverly in between the
-          barriers of progressive, fullon and forest. Balcosmos music was played
-          and appreciated across the world with recent releases on multiple
-          known psytrance labels, like IONO records, SOL Music and GreenTree
-          Records.
+          <Typography>
+            Originating from Ukraine, Alexey Romanov (38) is behind the solo
+            project “Balcosmos”. Influenced from a vast spectre of psytrance
+            sub-genres, including Goa, Forest, Fullon and Progressive, Alexey’s
+            initial Dj career evolved into the birth of his own music
+            productions under the project “Barbuix”. In 2017 he released his
+            debut album “Secret Ingredient” on the Russian based label Sound
+            Kitchen records and has since played on multiple parties and events
+            in countries such as Ukraine, Russia and Sweden.
+          </Typography>
+
+          <Typography>
+            In 2020 his developing skills and culmination of experiences, forged
+            his newly formed project Balcosmos, under which he is set to
+            reconnect the audience with a fresh music approach of pure
+            psychedelic vibes, balancing cleverly in between the barriers of
+            progressive, fullon and forest. Balcosmos music was played and
+            appreciated across the world with recent releases on multiple known
+            psytrance labels, like IONO records, SOL Music and GreenTree
+            Records.
+          </Typography>
         </Typography>
-      </Box>
+      </Grid>
     </Grid>
   );
 }
