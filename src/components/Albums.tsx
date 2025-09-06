8ef7f07d44cd592fcab2177b81d07dbcd5518ff6
babruix <romanalexey@gmail.com>
2025-09-06 13:37:57 +0200

    fix: increase YouTube iframe height for better visibility

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -135,7 +135,7 @@ export default function Albums() {
   <iframe
     src={getYouTubeEmbedUrl(modalData.youtube)}
     width="100%"
-    height="315"
+    height="400px"
     frameBorder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowFullScreen
